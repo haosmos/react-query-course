befe74219b160be47609902cbead91e362cda547
chaosmos <vitovich1987@gmail.com>
2023-02-09 23:41:03 +0300

    implemented the task search functionality

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { IssueItem } from './IssueItem';
 
 export default function IssuesList({ labels, status }) {
+  const [ searchValue, setSearchValue ] = useState('');
+  
   const issuesQuery = useQuery(
       [ 'issues', { labels, status } ],
       () => {
@@ -15,33 +18,95 @@ export default function IssuesList({ labels, status }) {
       }
   );
   
-  return (
-      <div>
-        <h2>Issues List</h2>
-        {issuesQuery.isLoading ? (
-            <p>Loading...</p>
-        ) : (
-             <ul className="issues-list">
-               {issuesQuery.data.map((issue) =>
-                   <IssueItem
-                       key={issue.id}
-                       title={issue.title}
-                       number={issue.number}
-                       assignee={issue.assignee}
-                       commentCount={issue.comments.length}
-                       createdBy={issue.createdBy}
-                       createdDate={issue.createdDate}
-                       labels={issue.labels}
-                       status={issue.status}
-                   />)
-               }
-             </ul>
-         )}
-        <ul>
-          <li>
-            <Link to="/issue/1">Issue 1</Link>
-          </li>
-        </ul>
-      </div>
+  const searchQuery = useQuery(
+      [ 'issues', 'search', searchValue ],
+      () =>
+          fetch(`/api/search/issues?q=${searchValue}`)
+              .then(res => res.json()),
+      {
+        enabled: searchValue.length > 0
+      }
   );
+  
+  return <div>
+    <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(event.target.elements);
+          setSearchValue(event.target.elements.search.value);
+        }}
+    >
+      <label htmlFor="search">Search issues</label>
+      <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {
+              setSearchValue('');
+            }
+          }}
+      />
+    </form>
+    
+    <h2>Issues List</h2>
+    {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+    ) : searchQuery.fetchStatus
+        === 'idle'
+        && searchQuery.isLoading
+        === true ? (
+            <ul className="issues-list">
+              {issuesQuery.data.map((issue) =>
+                  <IssueItem
+                      key={issue.id}
+                      title={issue.title}
+                      number={issue.number}
+                      assignee={issue.assignee}
+                      commentCount={issue.comments.length}
+                      createdBy={issue.createdBy}
+                      createdDate={issue.createdDate}
+                      labels={issue.labels}
+                      status={issue.status}
+                  />)
+              }
+            </ul>
+        ) : (
+            <>
+              <h2>Search Results</h2>
+              {
+                searchQuery.isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <>
+                      <p>{searchQuery.data.count} Results</p>
+                      <ul className="issues-list">
+                        {searchQuery.data.items.map(
+                            (issue) => (
+                                <IssueItem
+                                    key={issue.id}
+                                    title={issue.title}
+                                    number={issue.number}
+                                    assignee={issue.assignee}
+                                    commentCount={issue.comments.length}
+                                    createdBy={issue.createdBy}
+                                    createdDate={issue.createdDate}
+                                    labels={issue.labels}
+                                    status={issue.status}
+                                />
+                            )
+                        )}
+                      </ul>
+                    </>
+                )
+              }
+            </>
+        )}
+    {/* <ul> */}
+    {/*   <li> */}
+    {/*     <Link to="/issue/1">Issue 1</Link> */}
+    {/*   </li> */}
+    {/* </ul> */}
+  </div>;
 }
